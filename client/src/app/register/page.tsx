d4a00875b7ebb8cc4fba0f08643f14290fcd8f1e
Volodymyr Pavlenko <solaryasha@gmail.com>
2024-10-14 21:29:07 +0300

    add register page

---
+++ client/src/app/register/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { RegisterForm } from '@/components/register-form'
+
+export default function RegisterPage() {
+  
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <RegisterForm />
+    </div>
+  )
+}
