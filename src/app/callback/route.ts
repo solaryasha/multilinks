b44c1b05adec7497318ab6e7f07b2aaa0047e74a
Volodymyr Pavlenko <solaryasha@gmail.com>
2024-12-01 13:32:43 +0200

    add debug logs to callback route

--- src/app/callback/route.ts
+++ src/app/callback/route.ts
@@ -1,5 +1,13 @@
 import { handleAuth } from '@workos-inc/authkit-nextjs';
+import { NextRequest } from 'next/server';
 
-export const GET = handleAuth({
-  returnPathname: '/root',
-})
+export  const GET = async (request: NextRequest) => {
+  console.log('callback route was triggered', request);
+  try {
+    await handleAuth({
+      returnPathname: '/root',
+    })(request)
+  } catch(error) {
+    console.error(error);
+  }
+}
