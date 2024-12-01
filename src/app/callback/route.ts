1cc8da8155aa9f01e4e7d72ffb64ef337d0f9d6c
Volodymyr Pavlenko <solaryasha@gmail.com>
2024-12-01 13:41:31 +0200

    Revert "add debug logs to callback route"
    This reverts commit b44c1b05adec7497318ab6e7f07b2aaa0047e74a.

--- src/app/callback/route.ts
+++ src/app/callback/route.ts
@@ -1,13 +1,5 @@
 import { handleAuth } from '@workos-inc/authkit-nextjs';
-import { NextRequest } from 'next/server';
 
-export  const GET = async (request: NextRequest) => {
-  console.log('callback route was triggered', request);
-  try {
-    await handleAuth({
-      returnPathname: '/root',
-    })(request)
-  } catch(error) {
-    console.error(error);
-  }
-}
+export const GET = handleAuth({
+  returnPathname: '/root',
+})
