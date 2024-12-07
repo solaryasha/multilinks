aafd9cec8cff79c6ec29f7b55cb8e02056e4aa2f
Volodymyr Pavlenko <solaryasha@gmail.com>
2024-12-07 17:11:16 +0200

    initialize supabase client

--- src/app/my-links/page.tsx
+++ src/app/my-links/page.tsx
@@ -1,9 +1,24 @@
-import { Flex, Heading } from "@radix-ui/themes";
+import { createClient } from "@/utils/supabase/server";
+import { Button, Flex, Heading } from "@radix-ui/themes";
+import { withAuth } from "@workos-inc/authkit-nextjs";
 
-export default function MyLinksPage() {
+export default async function MyLinksPage() {
+  const { user } = await withAuth();
+  const supabase = await createClient();
+
+  const addLink = async () => {
+    if (user) {
+      await supabase.from("user_links").insert({
+        user_id: user.id,
+        link: "my link",
+      });
+    }
+  };
+
   return (
     <Flex align="center" justify="center">
       <Heading size="9">My links</Heading>
+      <Button onClick={addLink}></Button>
     </Flex>
   );
 }
